4cf566063912556dfe4ed61fe1fe1e1a6a050e0d
xin xu <74297674+sowsowiamme@users.noreply.github.com>
2025-10-21 21:30:40 +0800

    change the fit method

--- Logistic Regression/LogisticRegression.py
+++ Logistic Regression/LogisticRegression.py
@@ -22,20 +22,6 @@ class LogisticRegression:
         loss = -np.mean(math.log(p)*y + math.log(1-p)*(1-y))
         return loss
 
-    def fit(self,X,y):
-        X_b = np.c_(np.ones((X.shape[0],1)),X)
-        X_b = self.nomralize(X_b)
-        self.weights = np.zeros(X_b.shape[1])
-        for i in range(self.n_iters):
-            z = np.dot(X_b, self.weights)
-            p = self._sigmoid(-z)
-            gradient = X_b.T.dot(p-y) / len(y)
-            reg_term = self.lambd * self.weights / len(y)
-            reg_term[0] = 0
-            gradient += reg_term
-            self.weights -= self.learning_rate*gradient
-        return self
-
     def validate_input_array(self,X):
         if not isinstance(X, np.ndarray):
             X = np.array(X)
@@ -60,6 +46,20 @@ class LogisticRegression:
         z = np.dot(X_b, self.weights)
         p = self._sigmoid(z)
         return p
+        
+    def fit(self,X,y):
+        X_b = np.c_(np.ones((X.shape[0],1)),X)
+        X_b = self.nomralize(X_b)
+        self.weights = np.zeros(X_b.shape[1])
+        for i in range(self.n_iters):
+            z = np.dot(X_b, self.weights)
+            p = self._sigmoid(-z)
+            gradient = X_b.T.dot(p-y) / len(y)
+            reg_term = self.lambd * self.weights / len(y)
+            reg_term[0] = 0
+            gradient += reg_term
+            self.weights -= self.learning_rate*gradient
+        return self
 
     def preicit(self, X, threshold):
        return  self.cal_prob(X)>=threshold.astype('int')
